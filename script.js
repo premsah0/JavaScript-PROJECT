403ee33a7c3fd84ecd5b7fd399d1449acc187cd7
Prem Sah <iamprem2004@gmail.com>
2025-07-25 23:04:34 +0530

    js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-SVGLinearGradientElement
+<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.0 Transitional//EN”
+                      “https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd%E2%80%9D>
+<html xmlns=”https://www.w3.org/1999/xhtml%E2%80%9D>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+    <p id="ResultsP"></p>
+    <script type="text/javascript">
+             // Script Block 1
+              document.getElementById('ResultsP').innerHTML ='Hello World!';
+    </script>
+</body>
+<html></html>
